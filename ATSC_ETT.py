a103ed1271ef0f56c3096b221f4032aaa79dd807
Jordan W Johnson <jwllmjohnson@gmail.com>
2014-09-13 08:32:17 -0700

    Update ATSC_ETT.py
    Added comments and a horrible equation. :)

--- ATSC_ETT.py
+++ ATSC_ETT.py
@@ -1,35 +1,29 @@
 #
-# EPG | Automatically generated. DO NOT EDIT
+# ETT | Automatically generated. DO NOT EDIT
 #
-# A few things: Time as it pertains to 'start_time' is in GPS format. The  'length_in_seconds' means what you think it does -- the length in seconds. Examples provided. Make sure the shared values are the same as the ATSC document. Or else...
+# So, assuming your ptogram has an ETM_Location flag, the ATSC reciever will then go looking for accompnying ETM data. How exactly does it find it? Using the ETM_id. I seem to have the equation wrong, as there is an offset (descriptions assinged to the program behind it IICR) It goes like this:
 #
-from dvbobjects.PSI.ETT import *
-
-KCWQ_id = 27
-
-ett = extended_text_table_section(
-table_id = 0xCC,
-ETM_id = 0x1B0322, #add 4 each time
-source_id = KCWQ_id,
-ETT_table_id_extension = 300,
+# (event_id / 65536^2) + (source_id / 16386) * 4   | I'm not the best with math, so....Have fun.
+#
+# Anyway, once that is done, we get to something that should look familiar at this point: the multiple_string_structure. The difference here, is that we are using a much longer string. Up to ~248 characters here.
+ett1 = extended_text_table_section(
+table_id = 0xCC, #All EIT tables must have this table_id. ALL OF THEM.
+ETM_id = 140322, #See above.
+source_id = 20, #Channel source ID.
+ETT_table_id_extension = 700, #Should start this at 700.
 extended_text_message = multiple_string_structure(
 							number_strings = 0x1,
 							string_loop = [
 								string_loop_item(
-									ISO639_language_code = "eng",
+									ISO639_language_code = "eng", #This description is in English.
 									number_segments = 0x1,
 									segment_loop = [
 										segment_loop_item(
 											compression_type = 0x0,
 											mode = 0x0,
-											number_bytes = 0x11,
-											compressed_string = "\x4F\x6E\x65\x20\x64\x61\x79\x2C\x20\x4B\x69\x6E\x6F\x6D\x6F\x74\x6F\x20\x53\x61\x6B\x75\x72\x61\x2C\x20\x61\x20\x34\x74\x68\x20\x67\x72\x61\x64\x65\x72\x20\x73\x74\x75\x6D\x62\x6C\x65\x73\x20\x75\x70\x6F\x6E\x20\x74\x68\x65\x20\x6D\x79\x73\x74\x65\x72\x69\x6F\x75\x73\x20\x62\x6F\x6F\x6B\x20\x6F\x66\x20\x43\x6C\x6F\x77\x2E\x20\x55\x70\x6F\x6E\x20\x6F\x70\x65\x6E\x69\x6E\x67\x20\x69\x74\x20\x61\x6E\x64\x20\x72\x65\x61\x64\x69\x6E\x67\x20\x74\x68\x65\x20\x6E\x61\x6D\x65\x20\x6F\x66\x20\x54\x68\x65\x20\x57\x69\x6E\x64\x79\x20\x61\x6C\x6F\x75\x64\x2C\x20\x53\x61\x6B\x75\x72\x61\x20\x73\x63\x61\x74\x74\x65\x72\x73\x20\x74\x68\x65\x20\x63\x61\x72\x64\x73\x20\x74\x6F\x20\x74\x68\x65\x20\x77\x69\x6E\x64\x73\x2E\x20\x53\x61\x6B\x75\x72\x61\x20\x69\x73\x20\x65\x6C\x65\x63\x74\x65\x64\x20\x61\x6E\x64\x20\x61\x70\x70\x6F\x69\x6E\x74\x65\x64\x20\x62\x79\x20\x4B\x65\x72\x6F\x62\x65\x72\x6F\x73\x2C\x20\x47\x75\x61\x72",
-										),
-									],
-								),
-							],
-						),
+											number_bytes = 0x94,
+											compressed_string ="\x43\x6C\x61\x73\x73\x69\x63\x20\x63\x61\x72\x74\x6F\x6F\x6E\x73\x20\x66\x72\x6F\x6D\x20\x74\x68\x65\x20\x31\x39\x320\x60\x73\x2C\x20\x74\x6F\x20\x74\x68\x65\x20\x31\x39\x360\x60\x73\x2E",),],),],),
 version_number = 1,
-section_number = 0,
-last_section_number = 0,
+section_number = 1,
+last_section_number = 2,
 )
